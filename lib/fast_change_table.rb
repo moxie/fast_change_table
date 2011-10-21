259343bdf4349a934b260f279bbf5b710d15531c
Grady Griffin <gradyg@izea.com>
2011-10-21 00:09:57 -0400

    adding a disable keys option. indexes are removed and remembered until after data load then enabled

--- lib/fast_change_table.rb
+++ lib/fast_change_table.rb
@@ -7,25 +7,29 @@ module FastChangeTable
   end
   
   module ClassMethods
-    def fast_change_table(table_name, &block)
+    def fast_change_table(table_name, options = {}, &block)
+      options.symbolize_keys!
       old_table_name = "old_#{table_name}"
       rename_table(table_name, old_table_name)
       begin
          create_table_like(old_table_name, table_name, &block)
+         index_list = options[:disable_keys] ? disable_indexes(table_name) : []
          #prepare the columns names for the insert statements
          old = connection.columns(old_table_name).collect(&:name)
          current = connection.columns(table_name).collect(&:name)
          common = (current & old).sort
          columns_to_s = common.collect {|c| "`#{c}`"}.join(',')
-         execute "INSERT INTO #{table_name}(#{columns_to_s}) SELECT #{columns_to_s} FROM #{old_table_name}" 
+         execute "INSERT INTO #{table_name}(#{columns_to_s}) SELECT #{columns_to_s} FROM #{old_table_name}"
+         enable_indexes(table_name, index_list)
          drop_table(old_table_name)
-      rescue
+      rescue Exception => e
+        puts "#{e}\n#{e.backtrace}"
          drop_table(table_name) if table_exists?(table_name)
          rename_table(old_table_name, table_name)
       end
     end
-    
-    def self.create_table_like(like_table, table, &blk)
+
+    def create_table_like(like_table, table, &blk)
       code = table_schema_code(like_table)
       code.gsub!(/create_table\s+"#{like_table}"/, "create_table :#{table}")
       code.gsub!(/add_index\s+"#{like_table}"/, "add_index :#{table}")
@@ -36,12 +40,29 @@ module FastChangeTable
 
     private
 
-    def self.table_schema_code(table)
+    def table_schema_code(table)
       dumper = ActiveRecord::SchemaDumper.send(:new, connection)
       stream = StringIO.new
       dumper.table(table.to_s,stream)
       stream.rewind
       code = stream.read
+    end
+
+    def disable_indexes(table)
+      list = connection.indexes(table)
+      list.each do |i|
+        remove_index table, :name => i.name
+      end
+    end
+
+    def enable_indexes(table, list)
+      list.each do |i|
+        options = {}
+        options[:name]    = i.name    if i.name
+        options[:length]  = i.lengths if i.lengths
+        options[:unique]  = i.unique  if i.unique
+        add_index table, i.columns, :name => i.name
+      end
     end
   end
 end
